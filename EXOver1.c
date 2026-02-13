f531e585e1bf8e9abbc34709ab73d95745a72d8d
Adjamai <ndiayeadjamaimouna82@gmail.com>
2026-02-13 11:58:02 +0000

    correction

--- EXOver1.c
+++ EXOver1.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+int main (){
+    int x,n,som,cpt ;
+    float moy;
+    do{
+            printf("saisir n entiers : ");
+            scanf("%d" ,&n);
+    }while(n<0);
+    for(int i=0;i<n;i++){
+        do{
+            printf("saisir un entier negatif :");
+            scanf("%d",&x);
+        }while(x>0);
+        if (x%2==0){
+            som=som+x;
+           cpt++;
+        }
+    }
 
+    moy = som/cpt;
+    printf("la moyenne des nombre pair est de :%f ", moy);
+
+
+return 0 ;
+}
